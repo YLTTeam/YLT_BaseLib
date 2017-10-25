39d9d1c011d20c8c60eb1fdec0cde6007ec5d26d
QTEC <xiangph@qtec.cn>
2017-10-25 18:23:07 +0800

    release 0.0.3

--- YLT_BaseLib/Classes/NSObject+YLT_BaseObject.h
+++ YLT_BaseLib/Classes/NSObject+YLT_BaseObject.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "sys/utsname.h"
 
 @interface NSObject (YLT_BaseObject)
---
+++ YLT_BaseLib/Classes/YLT_BaseLib.h
@@ -0,0 +1,15 @@
+//
+//  YLT_BaseLib.h
+//  YLT_BaseLib
+//
+//  Created by YLT_Alex on 2017/10/25.
+//
+
+#ifndef YLT_BaseLib_h
+#define YLT_BaseLib_h
+
+#import "YLT_BaseMacro.h"
+#import "NSString+YLT_BaseString.h"
+#import "NSObject+YLT_BaseObject.h"
+
+#endif /* YLT_BaseLib_h */
