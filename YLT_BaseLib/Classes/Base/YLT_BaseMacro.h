4abf9aa295c03e3efdb1c3e56ecc7f894a293fe9
Alex <xphaijj0305@126.com>
2017-11-19 15:52:16 +0800

    release 0.0.21

--- YLT_BaseLib/Classes/Base/YLT_BaseMacro.h
+++ YLT_BaseLib/Classes/Base/YLT_BaseMacro.h
@@ -92,7 +92,7 @@
 //主bundle
 #define YLT_MainBundle [NSBundle mainBundle]
 
-#define YLT_WEAKSELF [self YLT_WeakSelf]
+#define YLT_WEAKSELF __weak typeof(self) weakSelf = self;
 
 //颜色宏定义
 #define YLT_RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
