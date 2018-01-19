f443d7931f0540c541949d1d542caa1d28bc48bf
QTEC <xiangph@qtec.cn>
2018-01-19 13:17:21 +0800

    no message

--- YLT_BaseLib/Classes/Base/YLT_BaseMacro.h
+++ YLT_BaseLib/Classes/Base/YLT_BaseMacro.h
@@ -96,7 +96,7 @@
 
 //颜色宏定义
 #define YLT_RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
-#define YLT_RGB(r,g,b) RGBA(r,g,b,1.0f)
+#define YLT_RGB(r,g,b) YLT_RGBA(r,g,b,1.0f)
 #define YLT_HEXCOLOR(hex) [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16)) / 255.0 green:((float)((hex & 0xFF00) >> 8)) / 255.0 blue:((float)(hex & 0xFF)) / 255.0 alpha:1]
 #define YLT_HEXCOLORA(hex, alpha) [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16)) / 255.0 green:((float)((hex & 0xFF00) >> 8)) / 255.0 blue:((float)(hex & 0xFF)) / 255.0 alpha:alpha]
 #define YLT_StringColor(color) [color YLT_ColorFromHexString]
