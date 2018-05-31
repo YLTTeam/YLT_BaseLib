e90846ee13c86fdff43db5b11cb79e8114031a69
相公 <xianggong@anve.com>
2018-05-31 10:05:51 +0800

    release 0.1.8

--- YLT_BaseLib.podspec
+++ YLT_BaseLib.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'YLT_BaseLib'
-  s.version          = '0.1.7'
+  s.version          = '0.1.8'
   s.summary          = 'YLT_BaseLib 基础类库.'
 
 # This description is used to generate tags and improve search results.
@@ -40,6 +40,7 @@ Pod::Spec.new do |s|
   # s.frameworks = 'UIKit', 'MapKit'
   s.dependency 'MJExtension'
   s.dependency 'FMDB'
-  s.dependency 'RMUniversalAlert'
+  s.dependency 'LGAlertView'
   s.dependency 'FastCoding'
+  
 end
--- YLT_BaseLib/Classes/Base/YLT_BaseMacro.h
+++ YLT_BaseLib/Classes/Base/YLT_BaseMacro.h
@@ -71,7 +71,7 @@
 #define YLT_DOCUMENT_PATH [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]
 #define YLT_CACHE_PATH [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]
 
-#define YLT_TipAlert(_S_, ...)     [[[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:(_S_), ##__VA_ARGS__] message:nil delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show]
+#define YLT_TipAlert(_S_, ...) [[LGAlertView alertViewWithTitle:[NSString stringWithFormat:(_S_), ##__VA_ARGS__] message:nil style:LGAlertViewStyleAlert buttonTitles:nil cancelButtonTitle:@"确定" destructiveButtonTitle:nil actionHandler:nil cancelHandler:nil destructiveHandler:^(LGAlertView * _Nonnull alertView) {}] show];
 
 #if DEBUG
 //输出日志信息
