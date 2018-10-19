cd0936ddcbd14d076132ad9083efae1a7200095a
相公 <xianggong@anve.com>
2018-10-19 17:26:32 +0800

    字典、数组打印为json字串格式

--- YLT_BaseLib/Classes/Category/NSArray+YLT_Log.m
+++ YLT_BaseLib/Classes/Category/NSArray+YLT_Log.m
@@ -8,21 +8,31 @@
 #import "NSArray+YLT_Log.h"
 
 @implementation NSArray (YLT_Log)
+#ifdef DEBUG
+- (NSString *)descriptionWithLocale:(id)locale {
+    return self.debugDescription;
+}
 
 /**
  改写数组的日志打印
-
+ 
  @param locale 本地化
  @return 打印的字串
  */
 - (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level {
-    NSMutableString *strM = [NSMutableString stringWithString:@"(\n"];
-    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        [strM appendFormat:@"\t%@,\n", obj];
-    }];
-    
-    [strM appendString:@")"];
-    return strM;
+    return self.debugDescription;
 }
 
+- (NSString *)debugDescription {
+    NSError *error = nil;
+    //字典转成json
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted  error:&error];
+    //如果报错了就按原先的格式输出
+    if (error) {
+        return [super debugDescription];
+    }
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    return jsonString;
+}
+#endif
 @end
