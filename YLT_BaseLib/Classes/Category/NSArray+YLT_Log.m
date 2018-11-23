c3387756b7e0014021d23c8b8596687ba760cf5f
相公 <xianggong@anve.com>
2018-11-23 10:20:25 +0800

    release 0.3.2

--- YLT_BaseLib/Classes/Category/NSArray+YLT_Log.m
+++ YLT_BaseLib/Classes/Category/NSArray+YLT_Log.m
@@ -9,10 +9,6 @@
 
 @implementation NSArray (YLT_Log)
 #ifdef DEBUG
-- (NSString *)descriptionWithLocale:(id)locale {
-    return self.debugDescription;
-}
-
 /**
  改写数组的日志打印
  
@@ -20,19 +16,13 @@
  @return 打印的字串
  */
 - (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level {
-    return self.debugDescription;
-}
-
-- (NSString *)debugDescription {
-    NSError *error = nil;
-    //字典转成json
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted  error:&error];
-    //如果报错了就按原先的格式输出
-    if (error) {
-        return [super debugDescription];
-    }
-    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    return jsonString;
+    NSMutableString *strM = [NSMutableString stringWithString:@"(\n"];
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [strM appendFormat:@"\t%@,\n", obj];
+    }];
+    
+    [strM appendString:@")"];
+    return strM;
 }
 #endif
 @end
--- YLT_BaseLib/Classes/Category/NSDictionary+YLT_Log.m
+++ YLT_BaseLib/Classes/Category/NSDictionary+YLT_Log.m
@@ -9,24 +9,19 @@
 
 @implementation NSDictionary (YLT_Log)
 #ifdef DEBUG
-- (NSString *)descriptionWithLocale:(id)locale {
-    return self.debugDescription;
-}
-
+/**
+ 改写字典的日志打印
+ 
+ @param locale 本地化
+ @return 打印的字串
+ */
 - (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level {
-    return self.debugDescription;
-}
-
-- (NSString *)debugDescription {
-    NSError *error = nil;
-    //字典转成json
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted  error:&error];
-    //如果报错了就按原先的格式输出
-    if (error) {
-        return [super debugDescription];
-    }
-    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    return jsonString;
+    NSMutableString *strM = [NSMutableString stringWithString:@"{\n"];
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [strM appendFormat:@"\t%@ = %@;\n", key, obj];
+    }];
+    [strM appendString:@"}\n"];
+    return strM;
 }
 #endif
 @end
