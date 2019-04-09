6f005fd73d0ef037f19f067c4c3547d05c05c43c
项工 <xianggong@anve.com>
2019-04-09 14:02:06 +0800

    release 0.3.34

--- YLT_BaseLib/Classes/Category/UIApplication+YLT_Extension.h
+++ YLT_BaseLib/Classes/Category/UIApplication+YLT_Extension.h
@@ -18,6 +18,10 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (BOOL)ylt_isMainQueue;
 
++ (void *)ylt_mainQueueKey;
+
++ (void *)ylt_mainQueueContext;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- YLT_BaseLib/Classes/Category/UIApplication+YLT_Extension.m
+++ YLT_BaseLib/Classes/Category/UIApplication+YLT_Extension.m
@@ -9,20 +9,31 @@
 
 @implementation UIApplication (YLT_Extension)
 
+static void *mainQueueKey = &mainQueueKey;
+static void *mainQueueContext = &mainQueueContext;
 /**
  是否是主队列
  
  @return 注意区分主队列与主线程
  */
 + (BOOL)ylt_isMainQueue {
-    static void *mainQueueKey = &mainQueueKey;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         dispatch_queue_set_specific(dispatch_get_main_queue(),
-                                    mainQueueKey, mainQueueKey, NULL);
+                                    mainQueueKey, mainQueueContext, NULL);
     });
     
     return dispatch_get_specific(mainQueueKey) == mainQueueKey;
+}
+
++ (void *)ylt_mainQueueKey {
+    [self ylt_isMainQueue];
+    return mainQueueKey;
+}
+
++ (void *)ylt_mainQueueContext {
+    [self ylt_isMainQueue];
+    return mainQueueContext;
 }
 
 @end
