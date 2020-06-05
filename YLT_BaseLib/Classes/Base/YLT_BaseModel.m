caf4ce83028ada16daf118800de88ceeb356c239
项工 <xiangpuhua@126.com>
2020-06-05 17:57:50 +0800

    no message

--- YLT_BaseLib/Classes/Base/YLT_BaseModel.m
+++ YLT_BaseLib/Classes/Base/YLT_BaseModel.m
@@ -24,8 +24,15 @@
 
 + (instancetype)mj_objectWithKeyValues:(NSMutableDictionary *)keyValues context:(NSManagedObjectContext *)context{
     YLT_BaseModel *res = [super mj_objectWithKeyValues:keyValues context:context];
-    if ((res.ylt_sourceData == nil) && [res respondsToSelector:@selector(setYlt_sourceData:)]) {
+    if ([res respondsToSelector:@selector(setYlt_sourceData:)]) {
         res.ylt_sourceData = keyValues.mutableCopy;
+    }
+    return res;
+}
+- (instancetype)mj_setKeyValues:(id)keyValues context:(NSManagedObjectContext *)context {
+    YLT_BaseModel *res = [super mj_setKeyValues:keyValues context:context];
+    if ([res respondsToSelector:@selector(setYlt_sourceData:)]) {
+        res.ylt_sourceData = keyValues;
     }
     return res;
 }
