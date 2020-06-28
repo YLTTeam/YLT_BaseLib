4f003118dacb6c9cc6288dd216cca729847141f0
项工 <xiangpuhua@126.com>
2020-06-28 10:25:34 +0800

    release 0.4.3

--- Example/Pods/Target Support Files/Pods-YLT_BaseLib_Example/Pods-YLT_BaseLib_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-YLT_BaseLib_Example/Pods-YLT_BaseLib_Example-frameworks.sh
@@ -198,14 +198,12 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FastCoding/FastCoding.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YLT_BaseLib/YLT_BaseLib.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FastCoding/FastCoding.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YLT_BaseLib/YLT_BaseLib.framework"
--- YLT_BaseLib.podspec
+++ YLT_BaseLib.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'YLT_BaseLib'
-  s.version          = '0.4.2'
+  s.version          = '0.4.3'
   s.summary          = 'YLT_BaseLib 基础类库.'
 
 # This description is used to generate tags and improve search results.
@@ -40,6 +40,5 @@ Pod::Spec.new do |s|
   s.frameworks = 'UIKit', 'MapKit', 'CoreTelephony'
   s.dependency 'MJExtension'
   s.dependency 'FMDB'
-  s.dependency 'FastCoding'
   s.dependency 'ReactiveObjC'
 end
