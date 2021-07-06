cb52303abbca8178d0e0081d2e1bd965e93243a1
Alex <xphaijj0305@126.com>
2021-07-06 16:06:07 +0800

    0.5.3

---
+++ YLT_BaseLib/Classes/Category/NSURL+YLT_Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSURL+YLT_Extension.h
+//  YLT_BaseLib
+//
+//  Created by Alex on 2021/7/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSURL (YLT_Extension)
+
+@end
+
+NS_ASSUME_NONNULL_END
